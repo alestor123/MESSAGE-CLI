7c27d8e44c073dbd485328aa5105c686e8cff8bf
alestor123 <alestoraldous@gmail.com>
2021-01-10 16:25:34 +0530

    working on config

--- App.js
+++ App.js
@@ -1,1 +1,7 @@
 #!/usr/bin/env node
+var Conf = require('conf'),
+config = new Conf()
+options = require('minimist')(process.argv.slice(2)),
+inId = options.i || options.id || process.env.SID,
+inNum = options.cn || options.cnumber || process.env.NUM,
+inKey = options.k || options.key || process.env.KEY;
