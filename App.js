cc6a32fb25cd68e6a72e790b71d191bf91242d74
alestor123 <alestoraldous@gmail.com>
2021-01-10 16:44:55 +0530

    almost

--- App.js
+++ App.js
@@ -2,6 +2,36 @@
 var Conf = require('conf'),
 config = new Conf()
 options = require('minimist')(process.argv.slice(2)),
+chalk = require('chalk'),
 inId = options.i || options.id || process.env.SID,
-inNum = options.cn || options.cnumber || process.env.NUM,
+twilio = require('twilio'),
+message = options.message || options.m || 'No Message',
+number = options.c || options.call || process.env.CALL,
+inNum = options.n || options.number || process.env.NUM,
 inKey = options.k || options.key || process.env.KEY;
+if(inId&&inKey&&inNum){
+console.log('Id : '+inId+' Key: '+inKey + 'Number : ' + inNum )
+    config.set('twilionum',inNum);
+    config.set('twilioid',inId);
+    config.set('twiliokey',inKey);
+    process.exit(0)
+}
+else if(check()) call()
+
+function check(){
+if(config.get('twilionum')&&config.get('twilioid')&&config.get('twiliokey')) return true
+else {
+console.log(chalk.red('Please set id and key and number'))
+return false}}
+
+function call(){
+var client = twilio(config.get('twilioid'),config.get('twiliokey'));
+client.messages
+  .create({
+     body: message,
+     from: config.get('twilionum'),
+     to: number
+   })
+
+console.log(`Number ${number} From : ${config.get('twilionum')} Message : ${message}`)
+}
